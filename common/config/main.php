64fe6772aec77e78195ce5a015384ea46057128d
masih <masih@masih-K42Jc>
2016-10-28 00:45:36 +0330

    using redis as cache and session storage

--- common/config/main.php
+++ common/config/main.php
@@ -4,9 +4,15 @@ return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
-            'class' => 'yii\caching\DbCache',
+            'class' => 'yii\redis\Cache',
+            'redis' => [
+                'hostname' => 'localhost',
+                'port' => 6379,
+                'database' => 0,
+            ]
         // default name of table is cache simply need to create cache table 
         // http://www.yiiframework.com/doc-2.0/yii-caching-dbcache.html#$cacheTable-detail    
+        //'class' => 'yii\caching\DbCache', 
         // 'db' => 'mydb',            
         //'cacheTable' => 'cache',           
         ],
@@ -19,9 +25,15 @@ return [
             'defaultRoles' => ['guest'],
         ],
         'session' => [
+           'class' => 'yii\redis\Session',
+            'redis' => [
+                'hostname' => 'localhost',
+                'port' => 6379,
+                'database' => 0,
+            ]            
         // default name of table is session simply need to create session table 
         // http://www.yiiframework.com/doc-2.0/yii-web-dbsession.html#$sessionTable-detail            
-            'class' => 'yii\web\DbSession',
+        //'class' => 'yii\web\DbSession',
         // 'db' => 'mydb',
         //'sessionTable' => 'session',
         ]
