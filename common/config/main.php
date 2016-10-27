2c4e65fe390f32f4374e0e4678679d763860c067
masih <masih@masih-K42Jc>
2016-10-27 17:27:39 +0330

    move session and cache to database

--- common/config/main.php
+++ common/config/main.php
@@ -1,17 +1,29 @@
 <?php
+
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
-            'class' => 'yii\caching\FileCache',
+            'class' => 'yii\caching\DbCache',
+        // default name of table is cache simply need to create cache table 
+        // http://www.yiiframework.com/doc-2.0/yii-caching-dbcache.html#$cacheTable-detail    
+        // 'db' => 'mydb',            
+        //'cacheTable' => 'cache',           
         ],
-        'authManager'=>[
+        'authManager' => [
             'class' => 'yii\rbac\DbManager',
             'itemTable' => 'auth_item',
             'itemChildTable' => 'auth_item_child',
             'assignmentTable' => 'auth_assignment',
             'ruleTable' => 'auth_rule',
             'defaultRoles' => ['guest'],
-        ],        
+        ],
+        'session' => [
+        // default name of table is session simply need to create session table 
+        // http://www.yiiframework.com/doc-2.0/yii-web-dbsession.html#$sessionTable-detail            
+            'class' => 'yii\web\DbSession',
+        // 'db' => 'mydb',
+        //'sessionTable' => 'session',
+        ]
     ],
 ];
