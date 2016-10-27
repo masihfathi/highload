8a4fabe84c219b9a3f9873ee81577e7fe9effa17
masih <masih@masih-K42Jc>
2016-10-27 15:08:24 +0330

    adding HTTP caching (client side caching to behavior) and fragment,pagecache

--- frontend/controllers/UserController.php
+++ frontend/controllers/UserController.php
@@ -12,19 +12,40 @@ use yii\filters\VerbFilter;
 /**
  * UserController implements the CRUD actions for User model.
  */
-class UserController extends Controller
-{
+class UserController extends Controller {
+
     /**
      * @inheritdoc
      */
-    public function behaviors()
-    {
+    public function behaviors() {
         return [
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
-                    'delete' => ['POST'],
+                    'delete' => ['post'],
                 ],
+            ],
+            [
+                'class' => 'yii\filters\PageCache',
+                'only' => ['view'],
+                'duration' => 60,
+                'variations' => [
+                    Yii::$app->request->get('id'),
+                ],
+                'dependency' => [
+                    'class' => 'yii\caching\DbDependency',
+                    'sql' => 'SELECT updated_at FROM user where id = :id',
+                    'params' => [':id' => \Yii::$app->request->get('id')],
+                ],
+            ],
+            [
+                'class' => 'yii\filters\HttpCache',
+                'only' => ['view'],
+                'lastModified' => function ($action, $params) {
+            $q = new \yii\db\Query();
+            return $q->select('updated_at')->from('user')
+                            ->where('id = :id', [':id' => Yii::$app->request->get('id')])->scalar();
+        },
             ],
         ];
     }
@@ -33,14 +54,13 @@ class UserController extends Controller
      * Lists all User models.
      * @return mixed
      */
-    public function actionIndex()
-    {
+    public function actionIndex() {
         $searchModel = new UserSearch();
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
         return $this->render('index', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
+                    'searchModel' => $searchModel,
+                    'dataProvider' => $dataProvider,
         ]);
     }
 
@@ -49,24 +69,22 @@ class UserController extends Controller
      * @param integer $id
      * @return mixed
      */
-    public function actionView($id)
-    {
+    public function actionView($id) {
         $cache = Yii::$app->cache;
         $secretKey = $cache['random'];
-        if ($secretKey === FALSE){
-            Yii::trace('Set secretkey in cache');
+        if ($secretKey === false) {
+            Yii::trace('set secret key in cache');
             $secretKey = Yii::$app->security->generateRandomKey();
-            $cache->set('random', $secretKey,10);
+            $cache->set('random', $secretKey, 10);
         }
-        $duration = 60;     // cache query results for 60 seconds.       
+
         $db = Yii::$app->db;
-        $model = $db->cache(function($db) use($id){
+        $model = $db->cache(function($db) use ($id) {
             return Yii::$app->controller->findModel($id);
-        },$duration);
-//        $model = $this->findModel($id);
+        });
         return $this->render('view', [
-            'model' => $model,
-            'key' => $secretKey,
+                    'model' => $model,
+                    'key' => $secretKey,
         ]);
     }
 
@@ -75,15 +93,14 @@ class UserController extends Controller
      * If creation is successful, the browser will be redirected to the 'view' page.
      * @return mixed
      */
-    public function actionCreate()
-    {
+    public function actionCreate() {
         $model = new User();
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
             return $this->redirect(['view', 'id' => $model->id]);
         } else {
             return $this->render('create', [
-                'model' => $model,
+                        'model' => $model,
             ]);
         }
     }
@@ -94,15 +111,14 @@ class UserController extends Controller
      * @param integer $id
      * @return mixed
      */
-    public function actionUpdate($id)
-    {
+    public function actionUpdate($id) {
         $model = $this->findModel($id);
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
             return $this->redirect(['view', 'id' => $model->id]);
         } else {
             return $this->render('update', [
-                'model' => $model,
+                        'model' => $model,
             ]);
         }
     }
@@ -113,8 +129,7 @@ class UserController extends Controller
      * @param integer $id
      * @return mixed
      */
-    public function actionDelete($id)
-    {
+    public function actionDelete($id) {
         $this->findModel($id)->delete();
 
         return $this->redirect(['index']);
@@ -127,12 +142,12 @@ class UserController extends Controller
      * @return User the loaded model
      * @throws NotFoundHttpException if the model cannot be found
      */
-    protected function findModel($id)
-    {
+    protected function findModel($id) {
         if (($model = User::findOne($id)) !== null) {
             return $model;
         } else {
             throw new NotFoundHttpException('The requested page does not exist.');
         }
     }
+
 }
